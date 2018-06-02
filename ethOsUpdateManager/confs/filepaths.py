7b652797ab0e667b681ea8e7fc3aa43329b24d04
Alexandre GAZAGNES <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-06-02 23:37:53 +0200

    Update filepaths.py

--- ethOsUpdateManager/confs/filepaths.py
+++ ethOsUpdateManager/confs/filepaths.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+
+
+# import 
+
+import os, subprocess, pickle, time
+
+from logging import debug, warning, info
+import logging
+
+# do not use pandas on ethos 1.3.1
+
+
+
+# Constants
+
+DATA_FOLDER = "/home/ethos/ethOS-update-manager/data/"
+debug(DATA_FOLDER)
+
+DATA_FILE = "update.csv" 
+debug(DATA_FILE) 
+
+COUNTER_FILE = ".counter" 
+debug(COUNTER_FILE) 
+
+TEMP_FILE = "update.temp" 
+debug(TEMP_FILE) 
