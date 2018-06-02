85e503ec39c33090e828b1573141e1dd0962ffd7
Alexandre GAZAGNES <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-06-02 23:34:45 +0200

    Create install.py

---
+++ ethOsUpdateManager/lib/install.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#--*--coding: utf-8 --*--
+
+
+def install():
+
+    with open("/home/ethos/ethOS-update-manager/ethuper/var/install.pk", "r") as f : v = f.read()
+      if v == "True" : 
+        os.system("rm -f /home/ethos/ethOS-update-manager/install")
+        with open("/home/ethos/ethOS-update-manager/ethuper/var/install.pk", "w") as f : f.write("False")
