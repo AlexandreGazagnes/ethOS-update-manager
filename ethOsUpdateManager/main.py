4e37c674b8b8d0ba786f9bde9fceadce5aeff279
Alexandre GAZAGNES <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-06-02 20:21:59 +0200

    Update main.py

--- ethOsUpdateManager/main.py
+++ ethOsUpdateManager/main.py
@@ -57,25 +57,25 @@ SLEEPER = 5 *60 # 5 minutes
 
 
 		# KEYS_SELECTED= [# global :
-		# 				'uptime','miner_secs', 					# time
-		# 				'miner_version','version',				# ethos
-		# 				'hostname','ip',   						# id 
-		# 				'cpu_temp','gpus','hash','proxy_problem'#status and perf 
+		# 				'uptime','miner_secs', 				# time
+		# 				'miner_version','version',			# ethos
+		# 				'hostname','ip',   				# id 
+		# 				'cpu_temp','gpus','hash','proxy_problem'	#status and perf 
 
-		# 				# for each GPU : 
+		# 		# for each GPU : 
 		# 				'miner_hashes','rejected_shares',		# perf
 		# 				'fanrpm','fanpercent','temp',			# temp
-		# 				'bioses',								# id 
-		# 				'core','mem','voltage',					# local config
-		# 				'watts',								# conso 
-		# 				'powertune']							# DEPRICIATED
+		# 				'bioses',					# id 
+		# 				'core','mem','voltage',				# local config
+		# 				'watts',					# conso 
+		# 				'powertune']					# DEPRICIATED
 
 
 		# HEADER = 		[ # global
 		# 				'timestamp', 'uptime','miner_secs','miner_version','version',				# ethos
 		# 				'hostname','ip', 'cpu_temp','gpus', 'working_gpus',
 		# 				'hash','proxy_problem',
-		# 				# for each GPU : 
+		# 			# for each GPU : 
 		# 				'miner_hashes','rejected_shares',
 		# 				'fanrpm','fanpercent','temp', 'temp_avg', 'temp_max',		
 		# 				'bioses','core','mem','voltage', 'watts', 'powertune']
