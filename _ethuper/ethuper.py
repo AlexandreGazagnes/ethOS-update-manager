06367c00d7c0227b49bd82c538313e369fbe1f47
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-06-03 13:28:20 +0200

    ethuper command

--- _ethuper/ethuper.py
+++ _ethuper/ethuper.py
@@ -3,41 +3,22 @@
 #--*-- coding: utf-8 --*--
 
 
+import argparse, os, system
 
-""""
-where COMMAND is :
+from ethuper.command import * 
+from ethuper.manager import * 
 
-    auto : updater auto launched or not when booting ethos + OPTION :
-        on : enable (DEFAULT and RECOMMENDED)
-        off : disable
 
-    start : start at command + OPTION :
-        fg : foreground, print out on stdout all info
-        bg : background, dont not show any info about logging (DEFAULT and RECOMMENDED)
 
-    stop : stop at command
+def main() : 
 
-    restart : stop and start at command to enable configs modification - RECOMMANDED after [CONFIG] - [SET] or [RESET]
+    # handle arg c/gar v
+    command, option = arg_manager()
 
-    config : manage configs + OPTION:
-        set : set specific config parametre(s)
-        reset : reset all parametres to orginal configuration
-        show : print out all parametres in use
+    command_manager(command, option)
 
-    reboot-aut : set auto reboot autorisation to enale automatic rig management mode. Rig will reboot when GPUs no detected/no working, hrate obvious problem, overwarming. (Full list in doc/) + OPTION:
-        on : enable (DEFAULT and RECOMMENDED)
-        off : disable
-        set : set specific auto reboot parametre(s) : temp, hrate...
-        reset : reset all parametres to orginal configuration
-        show : print out all parametres in use
 
-    merge-files : merge all update files
 
-    man : acces to manual (eg doc/)
+if __name__ == '__main__':
+    main()
 
-    unistall : uninstall entire programm, setting original conf and deletting dependencies + OPTION :
-        hard : -- WARNING -- delete all datafiles and log files
-        medium : unistall but Keep all datafiles and log files
-        soft : unistall but reinstall from scrach by saving data and logs files (DEFAULT and RECOMMENDED)
-
-""""
