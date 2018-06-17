01fb233bb2de012d91621fcc927e7d74e71227ee
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-06-17 18:10:53 +0200

    trying 3 to fix bugs for telegram api in BE

--- src/main.py
+++ src/main.py
@@ -182,6 +182,8 @@ def send_bot(msg="", token=TOKEN, chat_id=CHAT_ID):
 
 		req = str('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(chat_id) + '&parse_mode=Markdown&text=' + str(txt))
 
+		logging.warning(req)
+		
 		with urllib.request.urlopen(req) as f : 
 			none = f.read()
 	
