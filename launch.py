3b6766bd815570f6a93f9ccd4e6e3d93fa463d13
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-06-18 15:04:01 +0200

    split launch

--- launch.py
+++ launch.py
@@ -13,30 +13,15 @@ for more info  please read /src/main.py
 
 # import 
 
-import os, time, random
+from src._launch import *
 
 
 # main 
 
 def main() : 
-
-	# add a random sleeper to avoid multi boot 
-	s = round(random.random() * 10 * 1, 1) 
-	time.sleep(s)
-
-	# handle working processes 
-	process = os.popen("ps -aux | grep ethOS-update-manager").readlines()
-	is_working = ["src/main.py" in p for p in process]
-
-	# if 1 process break
-	if True in is_working :
-		print("ethOS-update-manager already running")
-		return 0
-	
-	# else launch one
-	print("ethOS-update-manager launched")
-	os.system("nohup python3 /home/ethos/ethOS-update-manager/src/main.py >> /home/ethos/ethOS-update-manager/logs/log 2>&1 &")
-	return 0
+	randomize_start()
+	is_working = working_process()
+	launch_if_needed(is_working)
 
 
 if __name__ == '__main__':
---
+++ src/_launch/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+launch functions
+"""
+
+
+# import 
+
+import os, time, random
+
+
+# functions
+
+def randomize_start() : 
+	"""add a random sleeper to avoid multi boot"""
+
+	s = round(random.random() * 10 * 1, 1) 
+	time.sleep(s)
+
+
+def working_process() : 
+	"""handle working processes"""
+
+	process = os.popen("ps -aux | grep ethOS-update-manager").readlines()
+	is_working = ["src/main.py" in p for p in process]
+
+	return is_working
+
+
+def launch_if_needed(is_working) : 
+	# if 1 process break
+	if True in is_working :
+		print("ethOS-update-manager already running")
+		return 0
+	
+	# else launch one
+	print("ethOS-update-manager launched")
+	os.system("nohup python3 /home/ethos/ethOS-update-manager/src/main.py >> /home/ethos/ethOS-update-manager/logs/log 2>&1 &")
+	return 0
+
+
