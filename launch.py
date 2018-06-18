bf669b59d1af89a7c6a110c1a957ff3a4fd96055
AlexGAzagnes <27285690+AlexandreGazagnes@users.noreply.github.com>
2018-06-18 12:00:04 +0200

    ok for final prod

--- launch.py
+++ launch.py
@@ -5,8 +5,8 @@
 """
 ethOS-update-manager - launcher - v0.5.0
 launcher for main.py
-avoid multi session launching by searching in all process
-if program aleardy running 
+avoid multi session launching by adding random sleeper  and
+by searching in all process if program aleardy running 
 for more info  please read /src/main.py
 """
 
